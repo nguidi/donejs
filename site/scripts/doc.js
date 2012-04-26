c5eabe3bac0802a8953bf7bfb5ca867a72549f87
David Luecke <daff@neyeon.com>
2012-04-26 14:06:51 -0600

    Documentation generation update

--- site/scripts/doc.js
+++ site/scripts/doc.js
@@ -1,15 +1,9 @@
-//steal/js jmvc/scripts/doc.js
-//load("jmvc/scripts/doc.js")
-
-
-
 load('steal/rhino/rhino.js');
 steal.overwrite = true;
 load('documentjs/documentjs.js');
 
 
 DocumentJS('site/scripts/doc.html',{
-	markdown : /*'jquery','steal','jmvc','tutorials','funcunit','mxui',*/
-		['can', 'site'],
+	markdown : ['steal', 'can', 'site', 'jquery', 'funcunit', 'tutorials' ],
 	out : 'site/docs'
 });
