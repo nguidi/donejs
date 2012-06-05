3a57b3f3cb167c328f5bd81a861fc5e42cd86514
German Cabral <gcabral@gmail.com>
2012-06-05 16:24:33 -0300

    Update gh-pages

--- vitco/mobile/js/transaccioneslist.js
+++ vitco/mobile/js/transaccioneslist.js
@@ -8,13 +8,31 @@ $('#transaccionListPage').bind('pageinit', function(event) {
 	getTransaccionList();
 });
 
+$.ajax({
+  url: url,
+  dataType: 'json',
+  data: data,
+  success: callback
+});
+
 function getTransaccionList() {
-	$.getJSON(serviceURL + 'gettransacciones.php', function(data) {
+	$.ajax({  url: serviceURL + 'gettransacciones.php',
+		  dataType: 'jsonp',
+		  success: function(data) {
+				$('#transaccionList li').remove();
+				transacciones = data.items;
+				$.each(transacciones, function(index, transaccion) {
+					$('#transaccionList').append('<li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-li-has-count ui-li-has-icon ui-btn-up-c"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><a href="index.html" class="ui-link-inherit"><h3 class="ui-li-heading">Remito Numero: ' + transaccion.remito + '</h3><p class="ui-li-desc">Cliente: ' +  transaccion.cliente + ' </p><p class="ui-li-desc">Chofer: ' +  transaccion.chofer + ' </p><p class="ui-li-desc">Producto: ' +  transaccion.producto  + ' </p></div></li>');
+				});
+				$('#transaccionList').listview('refresh');
+			}
+		});
+	/*.getJSON(serviceURL + 'gettransacciones.php', function(data) {
 		$('#transaccionList li').remove();
 		transacciones = data.items;
 		$.each(transacciones, function(index, transaccion) {
 			$('#transaccionList').append('<li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-li-has-count ui-li-has-icon ui-btn-up-c"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><a href="index.html" class="ui-link-inherit"><h3 class="ui-li-heading">Remito Numero: ' + transaccion.remito + '</h3><p class="ui-li-desc">Cliente: ' +  transaccion.cliente + ' </p><p class="ui-li-desc">Chofer: ' +  transaccion.chofer + ' </p><p class="ui-li-desc">Producto: ' +  transaccion.producto  + ' </p></div></li>');
 		});
 		$('#transaccionList').listview('refresh');
-	});
+	});*/
 }
