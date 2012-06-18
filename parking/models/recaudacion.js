a9222ada4bec8f293b494f60374641af53f68e49
Franco Soto <franco.soto.z@gmail.com>
2012-06-18 16:21:09 -0300

    trabaja

--- parking/models/recaudacion.js
+++ parking/models/recaudacion.js
@@ -19,18 +19,17 @@ steal('can/model')
      
     }, {})
     
-    can.Model("Cuenta_corriente_deb",{
-        findAll : 'GET /cuenta_corrientes',
+    can.Model("Autos",{
+        findAll : 'GET /recaudacions',
         models  : function(data){
             var newData = new Array()
-            $.each(data.items,function(index,elem){
+            $.each(data.items,function(i,elem){
                newData.push({
-                   id: elem.id,
-                   fecha: elem.fecha,
-                   debito: (elem.debcred == 1) ? elem.importe : 0,
-                   credito: (elem.debcred == 2) ? elem.importe : 0,
-                   detalle: elem.detalle
-               })
+                    id: elem.id,
+                    usuario_id: elem.usuario_id,
+                    fecha: elem.fecha,
+                    autos: elem.autos
+                })
             })
             return newData
         }     
