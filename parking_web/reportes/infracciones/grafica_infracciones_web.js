afd2efd37ec238bc8e6b3b6370a294035ca819d9
Franco Soto <franco.soto.z@gmail.com>
2012-08-03 21:31:36 -0300

    modificaciones en el reporte de infracciones, pasaje de datos

--- parking_web/reportes/infracciones/grafica_infracciones_web.js
+++ parking_web/reportes/infracciones/grafica_infracciones_web.js
@@ -21,12 +21,12 @@ steal(
                 this.element.html(can.view(url+'reportes/common/graf_reportes.ejs'))
                 var mes = 1//document.find('select#mes').val();
                 var anio = 2011//document.find('select#anio').val();
-                this.mostrar_grafica({muni_id: this.options.user.municipio, fecha: {mes: mes, anio: anio}});
+                this.mostrar_grafica({muni_id: this.options.user.municipio, data:{fecha: {mes: mes, anio: anio}}});
                 var self = this;
                 this.element.parents().find('select').bind('change', function(){
                     if($(this).attr('id') == 'mes') mes = $(this).val();
                     else anio = $(this).val()
-                    self.mostrar_grafica({muni_id: self.options.user.municipio, fecha: {mes: mes, anio: anio}});
+                    self.mostrar_grafica({muni_id: self.options.user.municipio, data:{ fecha: {mes: mes, anio: anio}}});
                 })
             },
             'mostrar_grafica': function(params){
@@ -37,7 +37,7 @@ steal(
                             data: $.map(obj,function(it){ return it.autos}),
                             name_x: 'Dias',
                             element_html: 'grafica_est',
-                            titulo_general: 'Cantidad de autos en infracción correspondiente al mes '+ array_mes[params.fecha.mes] +' del año '+ params.fecha.anio,
+                            titulo_general: 'Cantidad de autos en infracción correspondiente al mes '+ array_mes[params.data.fecha.mes] +' del año '+ params.data.fecha.anio,
                             subtitulo_general: '',
                             name_y: 'Cantidad de autos'
                             },
