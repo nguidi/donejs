2e4bb875fae9f0caac94f52692edafa97a888677
Franco Soto <franco.soto.z@gmail.com>
2012-08-07 17:05:41 -0300

    seccion de inspectores

---
+++ parking_web/inspectores/handler/inspectores.js
@@ -0,0 +1,44 @@
+steal(
+    'can/control/control.js',
+    'can/view/ejs',
+    'parking/fixtures/fixtures.js',
+    'parking/models/inspector.js',
+    'parking_web/inspectores/handler/inspectores.css',
+    'parking_web/config.js',
+    'bootstrap/js/bootstrap-tab.js',
+    'sigma/tabla/tabla.js')
+.then(
+    function(){
+        
+        can.Control("Inspectores",{
+            defaults: {
+                user: undefined
+            }
+        },{
+            'init': function( element , options ) {
+                this.element.html(can.view(url+'inspectores/handler/main.ejs'));
+                this.tabla = new Tabla("#tabla_inspectores",{
+                    head: url + 'inspectores/tabla/head.ejs',
+                    model: Inspector,
+                    row: url + 'inspectores/tabla/recipe.ejs',
+                    tableStyle: 'table inspectores'
+                });
+                
+            },
+            "ul.nav li click": function(el) { //Handler del evento de cambio de tab...
+                if (!el.hasClass('active')) {
+                    var toTab = this.element.find('div.tabbable ul.nav li.active').find('a').attr('href')
+                    var toUntab = el.find('a').attr('href')
+                    this.applyTab(toTab,toUntab)
+                }
+            },
+            'applyTab': function(toTab,toUntab) { // aplica cambios en el tab
+                this.element.find('div.tabbable ul.nav li.active').removeClass('active');
+                this.element.find('div.tab-content div.active').removeClass('active');
+                this.element.find('div.tab-content div#'+toUntab).addClass('active');
+                this.element.find('div.tabbable ul.nav li a[href='+toUntab+']').parent('li').addClass('active');
+            }
+        })
+    }
+);
+
