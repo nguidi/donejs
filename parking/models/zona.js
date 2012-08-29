f8feba40ce069abf1829e3c68a1047a70150dae8
Franco Soto <franco.soto.z@gmail.com>
2012-08-28 21:06:56 -0300

    arreglando modelos... conectando a backend / zona

--- parking/models/zona.js
+++ parking/models/zona.js
@@ -11,13 +11,40 @@ can.Model('Zona',
 /* @Static */
 {
         
-	findAll: "GET /zonas",
+	findAll: function (params, success, error) {
+            return $.ajax({
+                url: 'http://localhost/slim_php/backend_parking/zonas.php/zonas',
+                type: 'get',
+                dataType: 'json',
+                //data: JSON.stringify(params),
+                success: success,
+                error: error
+            });
+        },
   	findOne : "GET /zonas/{id}",
-  	create : "POST /zonas",
+  	create: function (params, success, error) {
+            return $.ajax({
+                url: 'http://localhost/slim_php/backend_parking/zonas.php/zonas',
+                type: 'post',
+                dataType: 'json',
+                data: JSON.stringify(params),
+                success: success,
+                error: error
+            });
+        },
  	update : "PUT /zonas/{id}",
-  	destroy : "DELETE /zonas/{id}",
+        destroy: function (params, success, error) {
+            return $.ajax({
+                url: 'http://localhost/slim_php/backend_parking/zonas.php/zonas/'+params,
+                type: 'delete',
+                dataType: 'json',
+                //data: JSON.stringify(params),
+                success: success,
+                error: error
+            });
+        },
         models  : function(data){
-            return data.items;
+            return data;
         } 
         
 },
