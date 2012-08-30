b9e225cf25ff04b4132fbb7123a9d9cc91c9c77e
Franco Soto <franco.soto.z@gmail.com>
2012-08-29 21:18:51 -0300

    modelo de inspector, falta updated

--- parking/models/inspector.js
+++ parking/models/inspector.js
@@ -13,11 +13,38 @@ steal('can/model')
  * Wraps backend candidato services.
  */
 can.Model("Inspector",{
-        findAll : 'GET /inspectors',
+        findAll: function (params, success, error) {
+            return $.ajax({
+                url: 'http://localhost/slim_php/backend_parking/inspectores.php/inspectores',
+                type: 'get',
+                dataType: 'json',
+                //data: JSON.stringify(params),
+                success: success,
+                error: error
+            });
+        },
         findOne : 'GET /inspectors/{id}',
-        create  : 'POST /inspectors',
+        create: function (params, success, error) {
+            return $.ajax({
+                url: 'http://localhost/slim_php/backend_parking/inspectores.php/inspectores',
+                type: 'post',
+                dataType: 'json',
+                data: JSON.stringify(params),
+                success: success,
+                error: error
+            });
+        },
         update  : 'PUT /inspectors/{id}',
-        destroy : 'DELETE /inspectors/{id}',
+        destroy: function (params, success, error) {
+            return $.ajax({
+                url: 'http://localhost/slim_php/backend_parking/inspectores.php/inspectores/'+params,
+                type: 'delete',
+                dataType: 'json',
+                //data: JSON.stringify(params),
+                success: success,
+                error: error
+            });
+        },
         models  : function(data){
             return data;
         }
